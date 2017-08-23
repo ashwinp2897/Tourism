be1398e7a7f44940c3c023d897439512432cbb65
ashwinp2897 <ashwinbrave2@gmail.com>
2017-08-23 17:04:43 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.aa;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+Button next;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        next=(Button)findViewById(R.id.button1);
+    }
+
+    
+}
